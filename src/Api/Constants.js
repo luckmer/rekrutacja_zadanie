e55d9398f87d5dbe064943701efa84a6de42737e
luckmer <woszczo04@gmail.com>
2021-07-15 21:59:55 +0200

    https links

---
+++ src/Api/Constants.js
@@ -0,0 +1,4 @@
+export const posts = "https://jsonplaceholder.typicode.com/posts";
+export const comments = "https://jsonplaceholder.typicode.com/comments";
+export const photos = "https://jsonplaceholder.typicode.com/photos";
+export const users = "https://jsonplaceholder.typicode.com/users";
