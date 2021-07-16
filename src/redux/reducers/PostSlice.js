0b696da6501a94c659a1e31e1c68725d8d538ec1
luckmer <woszczo04@gmail.com>
2021-07-16 10:14:46 +0200

    controll posts

--- src/redux/reducers/PostSlice.js
+++ src/redux/reducers/PostSlice.js
@@ -10,12 +10,50 @@ const initialState = {
   postDetail: [],
 };
 
+const fetchUserById = createAsyncThunk(
+  "videos/addVideo",
+  async ({ id, user }) => {
+    const data = typeof id === "number" ? id : "";
+    const result = FetchIdData(data, user);
+
+    return result;
+  }
+);
+
 const PostSlice = createSlice({
   name: "post",
 
   initialState,
 
-  reducers: {},
+  reducers: {
+    SetPosts: (state, action) => {
+      const posts = action.payload;
+      state.data = posts;
+    },
+
+    SetCommentDetail: (state, action) => {
+      const userPost = action.payload;
+      state.userComments.push(userPost);
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder.addCase(fetchUserById.fulfilled, (state, action) => {
+      const data = action.payload;
+
+      if (action.payload) {
+        const { post, photo, comments, user } = data;
+
+        state.commentDetail = comments;
+        state.photoDetail = photo;
+        state.postDetail = post;
+        state.userDetail = user;
+      }
+    });
+  },
 });
+
+export { fetchUserById };
+export const { SetPosts, SetCommentDetail } = PostSlice.actions;
 
 export default PostSlice.reducer;
