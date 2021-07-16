bcd25d8ab49d1de6d7c7c9c035696cdf6f144c17
luckmer <woszczo04@gmail.com>
2021-07-16 10:09:48 +0200

    pagination control

--- src/redux/reducers/PaginationSlice.js
+++ src/redux/reducers/PaginationSlice.js
@@ -11,7 +11,42 @@ const PaginationSlice = createSlice({
   name: "pagination",
   initialState,
 
-  reducers: {},
+  reducers: {
+    Next: (state, action) => {
+      const next = action.payload;
+      state.page = next;
+    },
+
+    Prev: (state, action) => {
+      const prev = action.payload;
+      state.page = prev;
+    },
+
+    MaxPage: (state, action) => {
+      const prev = action.payload;
+      state.maxPage = prev;
+    },
+
+    MinPage: (state, action) => {
+      const prev = action.payload;
+      state.minPage = prev;
+    },
+
+    SetNumber: (state, action) => {
+      const number = action.payload;
+      state.page = number;
+    },
+
+    Restart: (state) => {
+      state.page = 1;
+      state.limit = 9;
+      state.maxPage = 9;
+      state.minPage = 0;
+    },
+  },
 });
+
+export const { Next, Prev, SetNumber, MaxPage, MinPage, Restart } =
+  PaginationSlice.actions;
 
 export default PaginationSlice.reducer;
