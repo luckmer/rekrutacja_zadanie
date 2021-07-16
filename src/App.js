036a4e266bce17f4ea6466708c42df3cc91e89cd
luckmer <woszczo04@gmail.com>
2021-07-16 09:37:38 +0200

    connecting pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import NavBar from "./components/NavBar";
+import fetchApi from "./Api/fetchApi";
+import { useLocation, Switch, Route } from "react-router-dom";
+import { routes } from "./routes/Index";
+import "./css/App.css";
+
 const App = () => {
-  return <div> </div>;
+  const location = useLocation();
+  fetchApi();
+
+  return (
+    <main className="container">
+      <NavBar />
+      <Switch location={location} key={location.pathname}>
+        {routes.map(({ path, Component }) => (
+          <Route key={path} exact path={path} component={Component} />
+        ))}
+      </Switch>
+    </main>
+  );
 };
 
 export default App;
